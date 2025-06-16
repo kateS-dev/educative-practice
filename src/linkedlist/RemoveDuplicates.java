4769df3c2465a932a67ac9f3c0b2caa2ed547c1f
kate <192233029+kateS-dev@users.noreply.github.com>
2025-06-16 11:14:58 -0400

    feat: update RemoveDuplicates.java with alternative method

--- src/linkedlist/RemoveDuplicates.java
+++ src/linkedlist/RemoveDuplicates.java
@@ -21,7 +21,33 @@ public class RemoveDuplicates {
         }
         System.out.println("null");
     }
-    
+
+    /*
+    alternative answer:
+    public static LinkedListNode<Integer> removeDuplicates(LinkedListNode<Integer> head) {
+        // Check if list is empty or has only one node
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        LinkedListNode<Integer> outerNode = head;
+        // Traverse the linked list to find duplicates
+        while (outerNode != null) {
+            LinkedListNode<Integer> innerNode = outerNode;
+            while (innerNode != null && innerNode.next != null) {
+                if (outerNode.data.equals(innerNode.next.data)) {
+                    innerNode.next = innerNode.next.next;
+                } else {
+                    innerNode = innerNode.next;
+                }
+            }
+            outerNode = outerNode.next;
+        }
+
+        return head;
+    }
+    */
+
     public static LinkedListNode<Integer> removeDuplicates(LinkedListNode <Integer> head) {
         LinkedListNode<Integer> current = head;
         LinkedListNode<Integer> prev = null;
