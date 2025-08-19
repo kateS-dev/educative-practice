8e19521df1bcc7454239acf193e89284ecda4732
kate <192233029+kateS-dev@users.noreply.github.com>
2025-08-19 10:02:53 -0400

    feat: add one more method to solve it with less time complexity

--- src/graphs/FindMotherVertex/Solution2.java
+++ src/graphs/FindMotherVertex/Solution2.java
@@ -3,32 +3,43 @@ package graphs.FindMotherVertex;
 import java.util.Arrays;
 
 /*
-    this is O(V × (V+E)) for a full DFS from every vertex
+    this is O(V+E) for a full DFS from every vertex
 */
 
-public class Solution {
+/*
+    In a DFS forest, the mother vertex (if it exists) will always be in the last DFS tree finished.
+
+    Method:
+    1. Run a DFS over all vertices, keeping track of the last vertex you finish exploring
+    2. That vertex is your candidate mother vertex
+    3. Run DFS once more from that candidate to verify it reaches all vertices
+    4. If it does => candidate is mother vertex; otherwise, there's none
+*/
+
+public class Solution2 {
     public static int findMotherVertex(Graph g) {
         // use DFS
         LinkedList<Integer>[] list = g.adjacencyList;
         boolean[] visited = new boolean[list.length];
-        boolean[] candidates = new boolean[list.length];
-        Arrays.fill(candidates, true);
+        int candidates = -1;
+
+        // step1: find candidate (the last vertex never visit before, others will be overwritten)
         for (int i = 0; i < list.length; i++) {
-            dfsUtil(i, visited, list);
-            for (boolean item: visited) {
-                if (item == false) {
-                    candidates[i] = false;
-                }
+            if (!visited[i]) {
+                dfsUtil(i, visited, list);
+                candidates = i; // last finished vertex
             }
-            visited = new boolean[list.length];
         }
 
-        for (int i = 0; i < candidates.length; i++) {
-            if (candidates[i]) {
-                return i;
+        // step2: verify candidate (reset the visited array, use the candidate mother vertex to check if there is unvisited vertex)
+        Arrays.fill(visited, false);
+        dfsUtil(candidates, visited, list);
+        for (boolean v: visited) {
+            if (!v) {
+                return -1;
             }
         }
-        return -1;
+        return candidates;
     }
 
     private static void dfsUtil(int node, boolean[] visited, LinkedList<Integer>[] list) {
